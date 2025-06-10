4839182803fc9c4bead174c29b0c7357e029be65
parkjiwonn <monspirit47@gmail.com>
2025-06-11 02:48:54 +0900

    [feat] 구글 로그인 & 로그아웃

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,43 @@
-// app/page.tsx
-"use client";
+import { signOut } from "@/utils/supabase/actions";
+import { createClientForServer } from "@/utils/supabase/server";
+import Link from "next/link";
 
-import styles from "./page.module.scss";
-// import AuthForm from "@/app/components/Forms/AuthForm";
-// import { createClientForServer } from "@/utils/supabase/server";
+export default async function Home() {
+  const supabase = await createClientForServer();
 
-export default function Home() {
-  // const supabase = await createClientForServer();
+  const session = await supabase.auth.getUser();
 
-  // const session = await supabase.auth.getUser();
-
-  // console.log(session);
-  return (
-    <div className={styles.container}>
-      <div className={styles.content}>
-        <h1 className={styles.title}>TripPick</h1>
-        <p className={styles.subtitle}>
-          나만의 여행 일정을 쉽고 빠르게 계획해 보세요.
-        </p>
-        {/* <AuthForm /> */}
+  if (!session.data.user) {
+    return (
+      <div className="flex flex-col items-center justify-center h-screen gap-4">
+        <h1 className="text-4xl font-bold">Not Authenticated</h1>
+        <Link className="border rounded px-2.5 py-2" href="/auth">
+          Sign in
+        </Link>
       </div>
+    );
+  }
+
+  const {
+    data: {
+      user: { user_metadata, app_metadata },
+    },
+  } = session;
+
+  const { email } = user_metadata;
+
+  const Email = email ? `${email}` : "email Not Set";
+
+  console.log(session);
+  return (
+    <div className="flex flex-col gap-4">
+      <p className="text-xl">Email: {Email}</p>
+
+      <form action={signOut}>
+        <button className="border rounded px-2.5 py-2" type="submit">
+          Sign Out
+        </button>
+      </form>
     </div>
   );
 }
