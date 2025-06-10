d767951a2e7fbc94611d0f507cc4ef4b4abd88a4
parkjiwonn <monspirit47@gmail.com>
2025-06-11 01:24:28 +0900

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,25 @@
-import Image from "next/image";
-import variables from "./page.module.scss";
+// app/page.tsx
+"use client";
+
+import styles from "./page.module.scss";
+// import AuthForm from "@/app/components/Forms/AuthForm";
+// import { createClientForServer } from "@/utils/supabase/server";
 
 export default function Home() {
+  // const supabase = await createClientForServer();
+
+  // const session = await supabase.auth.getUser();
+
+  // console.log(session);
   return (
-    <div>
-      <main>
-        <h1 className={variables.title}>Hello</h1>
-      </main>
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>TripPick</h1>
+        <p className={styles.subtitle}>
+          나만의 여행 일정을 쉽고 빠르게 계획해 보세요.
+        </p>
+        {/* <AuthForm /> */}
+      </div>
     </div>
   );
 }
